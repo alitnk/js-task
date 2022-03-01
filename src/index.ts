2155b5e5fe20f7bdbbc0f3b42281d8f2fef4a4dd
Alireza Zamani <alirezazamani2922@gmail.com>
2022-03-01 18:32:08 +0330

    feat: Add basic features for getting the input

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,23 @@
-var foo: Array<string> = ["bar"];
-console.log(foo);
+import fs from "fs";
+export interface Operation {
+  date: String;
+  user_id: number;
+  user_type: "natural" | "juridical";
+  type: "cash_in" | "cash_out";
+  operation: {
+    amount: number;
+    currency: "EUR";
+  };
+}
+const args = process.argv.slice(2);
+
+if (args.length !== 1) {
+  throw Error(
+    "You should specify an input file for the script to work. for instance: node index.js input.json"
+  );
+}
+
+const inputFileName = args[0];
+
+const jsonContent = fs.readFileSync(inputFileName);
+const data: Operation[] = JSON.parse(jsonContent.toString()); // parsed data from the JSON file
