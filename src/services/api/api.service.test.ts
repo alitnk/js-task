ae784c419f1a2d4bd2a0a412db71ea99949f6cc4
Alireza Zamani <alirezazamani2922@gmail.com>
2022-03-01 18:48:04 +0330

    feat: Add cache layer to the API service
    Note that this is a minimal caching layer.
    In a real example this would perhaps need a revalidation timer.

--- src/services/api/api.service.test.ts
+++ src/services/api/api.service.test.ts
@@ -9,53 +9,89 @@ import { ApiService } from "./api.service";
 jest.mock("axios");
 const mockedAxios = axios as jest.Mocked<typeof axios>;
 
-test("cash-in route works", async () => {
-  const serverResponse: CashInResponse = {
-    data: {
-      percents: 0.03,
-      max: {
-        amount: 5,
-        currency: "EUR",
-      },
+const cashInServerResponse: CashInResponse = {
+  data: {
+    percents: 0.03,
+    max: {
+      amount: 5,
+      currency: "EUR",
     },
-  };
+  },
+};
 
-  mockedAxios.get.mockResolvedValue(serverResponse);
-
+test("cash-in route works", async () => {
+  mockedAxios.get.mockResolvedValue(cashInServerResponse);
   const apiService = new ApiService();
-  expect(await apiService.cashIn()).toBe(serverResponse.data);
+  expect(await apiService.cashIn()).toBe(cashInServerResponse.data);
 });
+
+test("cash-in uses caching", async () => {
+  const apiService = new ApiService();
+
+  mockedAxios.get.mockResolvedValue(cashInServerResponse);
+  const cashIn1 = await apiService.cashIn(); // First time, gets it from the API
+  mockedAxios.get.mockResolvedValue({ data: "totally inaccurate" });
+  const cashIn2 = await apiService.cashIn(); // Second time, gets it from the cache
+
+  expect(cashIn1).toEqual(cashIn2);
+});
+
+const cashOutNaturalServerResponse: CashOutNaturalResponse = {
+  data: {
+    percents: 0.3,
+    week_limit: {
+      amount: 1000,
+      currency: "EUR",
+    },
+  },
+};
 
 test("cash-out-natural route works", async () => {
-  const serverResponse: CashOutNaturalResponse = {
-    data: {
-      percents: 0.3,
-      week_limit: {
-        amount: 1000,
-        currency: "EUR",
-      },
-    },
-  };
-
-  mockedAxios.get.mockResolvedValue(serverResponse);
+  mockedAxios.get.mockResolvedValue(cashOutNaturalServerResponse);
 
   const apiService = new ApiService();
-  expect(await apiService.cashOutNatural()).toBe(serverResponse.data);
+  expect(await apiService.cashOutNatural()).toBe(
+    cashOutNaturalServerResponse.data
+  );
 });
 
-test("cash-out-juridical route works", async () => {
-  const serverResponse: CashOutJuridicalResponse = {
-    data: {
-      percents: 0.3,
-      min: {
-        amount: 0.5,
-        currency: "EUR",
-      },
-    },
-  };
+test("cash-out-natural uses caching", async () => {
+  const apiService = new ApiService();
 
-  mockedAxios.get.mockResolvedValue(serverResponse);
+  mockedAxios.get.mockResolvedValue(cashOutNaturalServerResponse);
+  const cashOutNatural1 = await apiService.cashOutNatural(); // First time, gets it from the API
+  mockedAxios.get.mockResolvedValue({ data: "totally inaccurate" });
+  const cashOutNatural2 = await apiService.cashOutNatural(); // Second time, gets it from the cache
+
+  expect(cashOutNatural1).toEqual(cashOutNatural2);
+});
+
+const cashOutJuridicalServerResponse: CashOutJuridicalResponse = {
+  data: {
+    percents: 0.3,
+    min: {
+      amount: 0.5,
+      currency: "EUR",
+    },
+  },
+};
+
+test("cash-out-juridical route works", async () => {
+  mockedAxios.get.mockResolvedValue(cashOutJuridicalServerResponse);
 
   const apiService = new ApiService();
-  expect(await apiService.cashOutJuridical()).toBe(serverResponse.data);
+  expect(await apiService.cashOutJuridical()).toBe(
+    cashOutJuridicalServerResponse.data
+  );
+});
+
+test("cash-out-juridical uses caching", async () => {
+  const apiService = new ApiService();
+
+  mockedAxios.get.mockResolvedValue(cashOutJuridicalServerResponse);
+  const cashOutJuridical1 = await apiService.cashOutJuridical(); // First time, gets it from the API
+  mockedAxios.get.mockResolvedValue({ data: "totally inaccurate" });
+  const cashOutJuridical2 = await apiService.cashOutJuridical(); // Second time, gets it from the cache
+
+  expect(cashOutJuridical1).toEqual(cashOutJuridical2);
 });
--- src/services/api/api.service.ts
+++ src/services/api/api.service.ts
@@ -37,27 +37,42 @@ export interface CashOutJuridicalResponse {
 export class ApiService {
   API_BASE = "https://developers.paysera.com/tasks/api";
 
+  cache = {
+    cashIn: null,
+    cashOutNatural: null,
+    cashOutJuridical: null,
+  };
+
   async cashIn(): Promise<CashInResponse["data"]> {
+    if (this.cache.cashIn) return this.cache.cashIn;
+
     const response = await httpClient.get<{}, CashInResponse>(
       `${this.API_BASE}/cash-in`
     );
 
+    this.cache.cashIn = response.data;
     return response.data;
   }
 
   async cashOutNatural(): Promise<CashOutNaturalResponse["data"]> {
+    if (this.cache.cashOutNatural) return this.cache.cashOutNatural;
+
     const response = await httpClient.get<{}, CashOutNaturalResponse>(
       `${this.API_BASE}/cash-out-natural`
     );
 
+    this.cache.cashOutNatural = response.data;
     return response.data;
   }
 
   async cashOutJuridical(): Promise<CashOutJuridicalResponse["data"]> {
+    if (this.cache.cashOutJuridical) return this.cache.cashOutJuridical;
+
     const response = await httpClient.get<{}, CashOutJuridicalResponse>(
       `${this.API_BASE}/cash-out-juridical`
     );
 
+    this.cache.cashOutJuridical = response.data;
     return response.data;
   }
 }
